660d6951f73d8c8b4338c220bbd0c5adda9185f7
Yohannees <yohannesgetachewerieso@gmail.com>
2023-01-10 15:05:33 +0300

    domino piling

--- domino_piling.py
+++ domino_piling.py
@@ -1,2 +1,12 @@
-def max_number_of_dominoes(M: int, N: int) -> int:
-    return (M * N) // 2
+def max_dominoes(M: int, N: int) -> int:
+    squares = M * N
+    if (M*N) % 2 == 0:
+        return squares // 2
+    else:
+        return (squares // 2) + (min(M, N) % 2)
+
+
+if __name__ == '__main__':
+    m, n = map(int, input("Enter the size of the board (M N): ").split())
+    result = max_dominoes(m, n)
+    print("Maximum number of dominoes that can be placed:", result)
