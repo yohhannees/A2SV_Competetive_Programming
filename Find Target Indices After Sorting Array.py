b5842c0ae4dcb6e58ba156c15e304f4105b0f422
Yohannees <yohannesgetachewerieso@gmail.com>
2023-01-12 16:25:48 +0300

    how many numbers are smaller

---
+++ Find Target Indices After Sorting Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        nums.sort()
+        ans = []
+        a = 0
+        b = 0
+        for i in nums:
+            a += 1
+            if i == target:
+                b = a - 1
+                ans.append(b)
+        return (ans)
