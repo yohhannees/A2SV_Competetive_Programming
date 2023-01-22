7f05f21f8928254c6e73b1b568e7bad6c0d6689d
Yohannees <yohannesgetachewerieso@gmail.com>
2023-01-22 16:39:37 +0300

    Runtime 767 ms Beats 45.81% Memory 30.4 MB Beats 46.86%

---
+++ 131. Palindrome Partitioning.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                result.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j + 1)
+                    part.pop()
+        dfs(0)
+        return result
+
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
