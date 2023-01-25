95a1cd69bae5d872dc115fa9db5f8b7d9913c75f
Yohannees <yohannesgetachewerieso@gmail.com>
2023-01-26 01:08:33 +0300

    Runtime 90 ms Beats 37.96% Memory 13.9 MB Beats 81.53%

---
+++ 2. Add Two Numbers.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    carry = 0
+
+    while carry or l1 or l2:
+      if l1:
+        carry += l1.val
+        l1 = l1.next
+      if l2:
+        carry += l2.val
+        l2 = l2.next
+      curr.next = ListNode(carry % 10)
+      carry //= 10
+      curr = curr.next
+
+    return dummy.next
